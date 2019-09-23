1f82adda3bd3c529bdb6246aa46f487e43b6964c
Julian Z <zapatajj@uninorte.edu.co>
2019-09-22 20:39:12 -0500

    Saca de base de datos

--- index.js
+++ index.js
@@ -36,57 +36,42 @@ app.get('/',(req,res)=>{
   })
 })
 
-//Otro Cambio jejee
 // Acceso a base de datos
 
 var mysql = require('mysql');
 var fs = require('fs');
 
-var con = mysql.createConnection({
-  host: "db4free.net",
-  user: "baenav",
-  password: "qwertyuiop",
-  database: "proyecto_diseno"
-});
-const quer1 = "SELECT P1, P2, P3, P4, UNIX_TIMESTAMP(CONCAT_WS(' ', fecha, hora)) AS datetime FROM datos ORDER BY fecha DESC, hora DESC LIMIT 100"
-con.connect(function(err) {
-  if (err) throw err;
-  con.query( quer1 , function (err, result, fields) {
+
+
+
+app.get('/Generar',function(req,res){
+
+  var con = mysql.createConnection({
+    host: "db4free.net",
+    user: "baenav",
+    password: "qwertyuiop",
+    database: "proyecto_diseno"
+  });
+  console.log('Generando')
+
+  const quer1 = "SELECT P1, P2, P3, P4, CONCAT_WS(' ', fecha, hora) AS datetime FROM datos ORDER BY fecha DESC, hora DESC LIMIT 500"
+
+  
+  con.connect(function(err) {
     if (err) throw err;
-    console.log(result);
-    
-    var cols= [{"label":"Date Time","type":"datetime"},{"label":"Potencia 1","type":"number"},{"label":"Potencia 2","type":"number"},{"label":"Potencia 3","type":"number"},{"label":"Potencia 4 ","type":"number"}]
-    var rows
-    
-    rows=[]
-    for (var i = 0; i < result.length; i++){
-      var rowi= {"c":[{"v":"Date(" +result[i].datetime+")"},{"v":result[i].P1},{"v":result[i].P2},{"v":result[i].P3},{"v":result[i].P4}]} 
-      rows.push(rowi)
-      
-    }
-    
-
-    table={cols:cols,rows:rows}
-    // console.log(table)
-
-    fs.writeFile('table.json', JSON.stringify(table), function (err) {
+    con.query( quer1 , function (err, result, fields) {
       if (err) throw err;
-      console.log('Saved!');
+      console.log(result);
+      
+      console.log(JSON.stringify(result))
+      res.send(JSON.stringify(result));
+      con.end();
       });
-    });
-});
+  }
+  );
+  
+})
 
-//function getDriver(callback) {
-//        connection.query("SELECT P1, P2, P3, P4, fecha, hora FROM datos ORDER BY fecha DESC, hora DESC LIMIT 10",
-//            function (err, rows) {
-//                //here we return the results of the query
-//                callback(err, rows);
-//            }
-//        );
-//}
-//var res = "0"
-//getDriver(function (err, driverResult){
-       //you might want to do something is err is not null...
-//      res.render('SQLtest', { 'title': 'SQL test',
-//      'result': driverResult});
-//    });
+
+
+
